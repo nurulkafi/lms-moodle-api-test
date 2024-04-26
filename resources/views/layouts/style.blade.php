28ca815877c45c2e45313f5fe2837436ed8938b2
nurulkafi <nurulkafi18@gmail.com>
2024-04-26 10:09:18 +0700

    asset template

---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,7 @@
+
+<link rel="stylesheet" href="{{ asset('mazer/assets/scss/app.scss')}}">
+<link rel="stylesheet" href="{{ asset('mazer/assets/scss/themes/dark/app-dark.scss')}}">
+<link rel="shortcut icon" href="{{ asset('mazer/assets/static/images/logo/favicon.svg')}}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('mazer/assets/static/images/logo/favicon.png')}}" type="image/png">
+
+<link rel="stylesheet" href="{{ asset('mazer/assets/scss/iconly.scss')}}">
